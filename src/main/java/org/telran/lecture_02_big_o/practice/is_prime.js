13cda26afbf803ea24bd1a3b101cfeb8e4902cd0
Sergejlynnyk <hochzeitinfrankfurt@gmail.com>
2025-02-10 13:10:59 +0100

    Update is_prime.js

--- src/main/java/org/telran/lecture_02_big_o/practice/is_prime.js
+++ src/main/java/org/telran/lecture_02_big_o/practice/is_prime.js
@@ -6,8 +6,19 @@
 //     Если число делится на какое-то из этих чисел, то оно не простое.
 //     Если ни одно из чисел не является делителем, то число простое.
 
-function is_prime(number) {
-    // You code ...
+    function is_prime(number) {
+    if (number < 2) return false;
+    if (number === 2) return true;
+    if (number % 2 === 0) return false;
+    
+    for (let i = 3; i * i <= number; i += 2) {
+        if (number % i === 0) return false;
+    }
+    return true;
+}
+
+console.log(is_prime(200)); 
+
 }
 
 // Оценить сложность алгоритма
