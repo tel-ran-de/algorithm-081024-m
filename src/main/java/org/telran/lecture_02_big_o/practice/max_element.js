8600517e2ef46336eef05747ae4f47eeb8320719
IrinAnoVo <irinapavelviktor@gmail.com>
2025-02-11 09:50:46 +0100

    Update max_element.js

--- src/main/java/org/telran/lecture_02_big_o/practice/max_element.js
+++ src/main/java/org/telran/lecture_02_big_o/practice/max_element.js
@@ -7,7 +7,15 @@
 //     Вернуть наибольший элемент.
 
 function max_element(numbers) {
-    // You code ...
+       let max = number[0]
+    for (let i = 0; i < numbers.length; i++) {
+        if(max < numbers[i]) {
+            max = numbers[i]
+        }
+    } 
+    return max  
+}
+console.log(max_element(numbers));
 }
 
-// Оценить сложность алгоритма
+// Оценить сложность алгоритма 0(n);
