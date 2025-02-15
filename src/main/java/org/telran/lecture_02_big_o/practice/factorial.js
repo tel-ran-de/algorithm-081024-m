4c3792f66bded92c65166a4d50540e46bd80b496
IrinAnoVo <irinapavelviktor@gmail.com>
2025-02-15 01:30:55 +0100

    Update factorial.js

--- src/main/java/org/telran/lecture_02_big_o/practice/factorial.js
+++ src/main/java/org/telran/lecture_02_big_o/practice/factorial.js
@@ -6,7 +6,13 @@
 //     Вернуть результат.
 
 function factorial(number) {
-    // You code ...
+    let result = 1
+    for (let i = 1; i <= number; i++) {
+        result *= i
+    }
+    return result
+}
+console.log(factorial(4));
 }
 
-// Оценить сложность алгоритма
+// Оценить сложность алгоритма 0(1);
