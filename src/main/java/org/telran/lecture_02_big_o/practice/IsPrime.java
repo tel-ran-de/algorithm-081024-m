0a3891ff0ba9b0d199b82ca9a8267d62e6458c89
Natalia Kondratenko <62788967+NatKond@users.noreply.github.com>
2025-02-16 23:27:49 +0100

    Update IsPrime.java
    Homework 2 part 2

--- src/main/java/org/telran/lecture_02_big_o/practice/IsPrime.java
+++ src/main/java/org/telran/lecture_02_big_o/practice/IsPrime.java
@@ -10,14 +10,26 @@ package org.telran.lecture_02_big_o.practice;
 
 public class IsPrime {
     public static void main(String[] args) {
+        System.out.println("isPrime(1) = " + isPrime(1));
+        System.out.println("isPrime(2) = " + isPrime(2));
+        System.out.println("isPrime(3) = " + isPrime(3));
+        System.out.println("isPrime(9) = " + isPrime(9));
+        System.out.println("isPrime(9) = " + isPrime(427));
 
     }
 
     public static boolean isPrime(int number) {
-        // You code ...
+        if (number != 2 && number % 2 == 0) {
+            return false;
+        }
+        for (int i = 3; i <= Math.sqrt(number); i += 2) {
+            if (number != i && number % i == 0) {
+                return false;
+            }
+        }
         return true;
     }
 }
 
 
-// Оценить сложность алгоритма
+// Оценить сложность алгоритма O(log(n))
