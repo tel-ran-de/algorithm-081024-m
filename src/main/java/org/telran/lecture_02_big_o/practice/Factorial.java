cb102964b9d838d56d723deb9f33fdda51c87f1b
Natalia Kondratenko <62788967+NatKond@users.noreply.github.com>
2025-02-16 23:23:45 +0100

    Update Factorial.java
    Homework 2 Part 1

--- src/main/java/org/telran/lecture_02_big_o/practice/Factorial.java
+++ src/main/java/org/telran/lecture_02_big_o/practice/Factorial.java
@@ -9,13 +9,27 @@ package org.telran.lecture_02_big_o.practice;
 
 public class Factorial {
     public static void main(String[] args) {
-
+        System.out.println(factorial(5));
+        System.out.println(factorial(0));
+        System.out.println(factorial(15));
+        System.out.println(factorialRecursion(5));
+        System.out.println(factorialRecursion(0));
+        System.out.println(factorialRecursion(15));
     }
 
     public static int factorial(int n) {
-        // You code ...
-        return 0;
+        if (n == 0) return 1;
+        int result = 1;
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+    public static int factorialRecursion(int n) {
+        if (n == 1 || n == 0) return 1;
+        return n * factorial(n - 1);
     }
 }
 
-// Оценить сложность алгоритма
+// Оценить сложность алгоритма O(n)
