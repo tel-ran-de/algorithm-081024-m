d86837464cdb6612aec95bcc67cc383c49183bfa
Tati <tatka.bogat@gmail.com>
2025-02-16 22:13:33 +0100

    add homework task1

--- src/main/java/org/telran/lecture_02_big_o/practice/LinearSearch.java
+++ src/main/java/org/telran/lecture_02_big_o/practice/LinearSearch.java
@@ -9,14 +9,22 @@ package org.telran.lecture_02_big_o.practice;
 
 public class LinearSearch {
     public static void main(String[] args) {
-
+        int[] array = {2, 6, 2, 1, 9, 22, -3, 4, 7};
+        int num = 4;
+        System.out.println(linearSearch(array, num));
     }
 
     public static int linearSearch(int[] numbers, int target_number) {
         // You code...
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == target_number) {
+                return i;
+            }
+        }
         return -1;
     }
 }
 
 
 // Оценить сложность алгоритма
+// Сложность константная линейная O(n) - единоразовый перебор всех элементов
