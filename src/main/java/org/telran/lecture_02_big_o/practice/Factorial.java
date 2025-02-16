a61cc41d9d92e44404b763562d2ebd6fc869c16e
LiudmylaIermolenko <lussi.yer@gmail.com>
2025-02-16 18:31:56 +0100

    Update Factorial.java

--- src/main/java/org/telran/lecture_02_big_o/practice/Factorial.java
+++ src/main/java/org/telran/lecture_02_big_o/practice/Factorial.java
@@ -9,13 +9,20 @@ package org.telran.lecture_02_big_o.practice;
 
 public class Factorial {
     public static void main(String[] args) {
-
+    int number = 5; // Заданное число
+        System.out.println("Факториал " + number + " = " + factorial(number));
     }
 
     public static int factorial(int n) {
-        // You code ...
-        return 0;
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
     }
 }
 
 // Оценить сложность алгоритма
+//В каждой итерации происходит умножение и присваивание (O(1) операция).
+//Общая временная сложность: O(n) (линейная сложность).
+//Используется только одна переменная result, не зависит от n.
