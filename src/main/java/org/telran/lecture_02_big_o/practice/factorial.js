97760e554e095fe991449b8dec03e8ad8a0b3a10
ViktoriaLadyhina <vika.ladyhina@gmail.com>
2025-02-17 09:00:06 +0100

    Update factorial.js

--- src/main/java/org/telran/lecture_02_big_o/practice/factorial.js
+++ src/main/java/org/telran/lecture_02_big_o/practice/factorial.js
@@ -6,7 +6,15 @@
 //     Вернуть результат.
 
 function factorial(number) {
-    // You code ...
+
+    let tmp = 1;
+
+    for (let i = 1; i <= number; i++) {
+        tmp *= i;
+    }
+    console.log(tmp);
 }
+
+factorial(6)
 
 // Оценить сложность алгоритма
