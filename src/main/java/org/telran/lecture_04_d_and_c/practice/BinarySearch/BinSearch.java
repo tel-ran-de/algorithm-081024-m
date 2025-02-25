bccb9ebc5db01373a8332b6d3258956d34f57e01
boo <booblegum42@gmail.com>
2025-02-26 00:30:42 +0545

    BinarySearch solution

--- src/main/java/org/telran/lecture_04_d_and_c/practice/BinarySearch/BinSearch.java
+++ src/main/java/org/telran/lecture_04_d_and_c/practice/BinarySearch/BinSearch.java
@@ -9,8 +9,9 @@ package org.telran.lecture_04_d_and_c.practice.BinarySearch;
 public class BinSearch {
 
     public static void main(String[] args) {
+        //           0  1  2  3   4    5  6   7   8   9
         int[] arr = {2, 5, 8, 12, 16, 23, 38, 56, 72, 91};
-        int target = 23;
+        int target = 21;
         int result = binarySearchRecursive(arr, target, 0, arr.length - 1);
 
         if (result == -1) {
@@ -21,8 +22,21 @@ public class BinSearch {
     }
 
     public static int binarySearchRecursive(int[] arr, int target, int left, int right) {
-        // You code ...
-        return -1;
+        int middle = (left + right) / 2;
+        if (arr[middle] == target) {
+            return middle;
+        }
+        if (left >= right) {
+            return -1;
+        }
+        if (arr[middle] < target) {
+            // работаем с правой частью
+            return binarySearchRecursive(arr, target, middle + 1, right);
+        } else {
+            // работаем с левой частью
+            return binarySearchRecursive(arr, target, left, middle - 1);
+        }
+
     }
 
 
