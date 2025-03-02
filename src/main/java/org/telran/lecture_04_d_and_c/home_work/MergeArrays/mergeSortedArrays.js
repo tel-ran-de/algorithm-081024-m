0f37bc9c5bd0cf38a147cd134889a543e1e5b491
Halaktionova Olha <galaktionovaolga555@gmail.com>
2025-03-02 14:18:05 +0200

    Update mergeSortedArrays.js

--- src/main/java/org/telran/lecture_04_d_and_c/home_work/MergeArrays/mergeSortedArrays.js
+++ src/main/java/org/telran/lecture_04_d_and_c/home_work/MergeArrays/mergeSortedArrays.js
@@ -24,10 +24,32 @@
 //Вход: arr1 = [], arr2 = [1, 2, 3]
 //Выход: mergedArray = [1, 2, 3]
 
-function mergeSortedArrays(arr1, arr2) {
-    // You code ...
+function mergeSortedArrays (arr1, arr2){
+    let mergedArray = []
+    i = 0
+    j = 0
+
+while(i < arr1.length && j <arr2.length){
+    if (arr1[i] <= arr2[j]){
+        mergedArray.push(arr1[i])
+        i++
+    }else{
+        mergedArray.push(arr2[j])
+        j++
+    }
 }
 
-const arr1 = [1, 3, 5, 7];
-const arr2 = [2, 4, 6, 8];
-const mergedArray = mergeSortedArrays(arr1, arr2);
+while(i < arr1.length){
+    mergedArray.push(arr1[i])
+    i++
+}
+while (j < arr2.length) {
+    mergedArray.push(arr2[j]);
+    j++;
+}
+return mergedArray
+}
+
+console.log(mergeSortedArrays([1, 3, 5, 7], [2, 4, 6, 8])); 
+console.log(mergeSortedArrays([1, 2, 2, 5], [1, 3, 4])); 
+console.log(mergeSortedArrays([], [1, 2, 3]));
