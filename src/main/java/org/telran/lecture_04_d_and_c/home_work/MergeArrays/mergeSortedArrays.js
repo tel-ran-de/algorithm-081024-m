5c4ec240f216ff5332585528d789f3a833d129f2
ViktoriaLadyhina <vika.ladyhina@gmail.com>
2025-03-03 08:03:36 +0100

    Update mergeSortedArrays.js

--- src/main/java/org/telran/lecture_04_d_and_c/home_work/MergeArrays/mergeSortedArrays.js
+++ src/main/java/org/telran/lecture_04_d_and_c/home_work/MergeArrays/mergeSortedArrays.js
@@ -25,7 +25,27 @@
 //Выход: mergedArray = [1, 2, 3]
 
 function mergeSortedArrays(arr1, arr2) {
-    // You code ...
+    const mergeArray = []
+    let i = 0;
+    let j = 0;
+    while (i < arr1.length && j < arr2.length) {
+        if (arr1[i] < arr2[j]) {
+            mergeArray.push(arr1[i]);
+            i++;
+        } else {
+            mergeArray.push(arr2[j]);
+            j++;
+        }
+    }
+    while (i < arr1.length) {
+        mergeArray.push(arr1[i]);
+        i++;
+    }
+    while (j < arr2.length) {
+        mergeArray.push(arr2[j]);
+        j++;
+    }
+    return mergeArray;
 }
 
 const arr1 = [1, 3, 5, 7];
