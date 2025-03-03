23df0ab99aa655c58745a83b967a9ab3616687a1
ViktoriaLadyhina <vika.ladyhina@gmail.com>
2025-03-03 12:19:46 +0100

    Update Task01.js

--- src/main/java/org/telran/lecture_05_merge/practice/tasks/Task01.js
+++ src/main/java/org/telran/lecture_05_merge/practice/tasks/Task01.js
@@ -4,3 +4,17 @@
 // Примечание: Используйте алгоритм сортировки, если нужно.
 
 let numbers = [2, 4, -8, -5, 0, 7, 12, 0, -4, 10]
+
+function sumLargest(array) {
+    if (array.length === 0) {
+        return 0
+    }
+    let sortedArrays = mergeSort(array)
+    let sum = 0;
+    let count = 0;
+    for (let i = Math.max(0, sortedArrays.length - 5); i < sortedArrays.length; i++) {
+        sum += sortedArrays[i];
+        count++;
+    }
+    return sum;
+}
