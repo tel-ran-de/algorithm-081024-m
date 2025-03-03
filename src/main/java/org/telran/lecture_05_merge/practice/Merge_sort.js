d2e8ce1c278bccc5ca8faab82e3d4621e624c6c8
helenkonovalova <37671989+helenkonovalova@users.noreply.github.com>
2025-03-03 12:29:41 +0200

    Update Merge_sort.js

--- src/main/java/org/telran/lecture_05_merge/practice/Merge_sort.js
+++ src/main/java/org/telran/lecture_05_merge/practice/Merge_sort.js
@@ -12,7 +12,21 @@ function mergeSort(arr) {
 
 function merge(left, right) {
     let result = [];
-    // TODO: добавьте реализацию методы
+    let index1 = 0
+    let index2 = 0
+    while(index1 < left.length && index2 < right.length){
+        if (left[index1] < right[index2] ) {
+            result.push(left[index1++])
+        } else {
+            result.push(right[index2++])
+        }
+    }
+    while (index1 < left[index1]) {
+        result.push(left[index1++])
+    }
+    while (index2 < right[index2]) {
+        result.push(right[index2++])
+    }
     return result;
 }
 
