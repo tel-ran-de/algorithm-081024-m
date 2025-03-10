d73fda0fae935ed12e941ec54e2204229aed14a6
Sergej Hofmann <152594101+HofmannS@users.noreply.github.com>
2025-03-10 12:23:21 +0100

    Update Task03.js

--- src/main/java/org/telran/lecture_06_quick/tasks/Task03.js
+++ src/main/java/org/telran/lecture_06_quick/tasks/Task03.js
@@ -13,7 +13,20 @@
 // 5
 
 let results = [1, 3, 4, 3, 5, 6, 7, 7, 6, 1]
-
+//Вариант 1
+function findWinners(array){
+    let sortedResults = array.sort((a, b) => b - a);
+    let winnersCount = 0
+    for (let i = 0; i < sortedResults.length; i++) {
+        if(array[i] < array[0] - 2){
+            winnersCount++
+        }
+    }
+    return winnersCount
+}
+let winners = findWinners(results)
+console.log(`Количество призеров: ${winners}`);
+//Вариант 2
 function findWinners(array){
     let firstPlaceResult = 0
     let winnersCount = 0
