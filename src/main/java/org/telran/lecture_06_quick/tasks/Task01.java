bc00673bb76bbdcfa042c83db54791625c6466b8
boo <booblegum42@gmail.com>
2025-03-10 13:59:21 +0545

    materials to lecture_06

---
+++ src/main/java/org/telran/lecture_06_quick/tasks/Task01.java
@@ -0,0 +1,15 @@
+package org.telran.lecture_06_quick.tasks;
+
+import java.util.Arrays;
+
+// "Простая сортировка"
+// Дан массив произвольных целых чисел
+// Используя любой алгоритм сортировки из lecture_6, отсортируйте элементы в порядке возрастания.
+
+
+public class Task01 {
+    public static void main(String[] args) {
+        int[] numbers = {2, 4, 8, -5, 0, 7, 12, 0, -4, 10};
+
+    }
+}
