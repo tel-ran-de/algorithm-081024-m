792e7856c91a816153e47d2e2ccc645f93de65c1
Sergej Hofmann <152594101+HofmannS@users.noreply.github.com>
2025-03-10 11:22:29 +0100

    Update Task01.js

--- src/main/java/org/telran/lecture_06_quick/tasks/Task01.js
+++ src/main/java/org/telran/lecture_06_quick/tasks/Task01.js
@@ -3,3 +3,35 @@
 // Используя любой алгоритм сортировки из lecture_6, отсортируйте элементы в порядке возрастания.
 
 const numbers = [3, -5, 0, 4, 12, -2, 10];
+function quickSort(array, start, end) {
+    if (start < end) {
+        let indexPivot = partition(array, start, end);
+        // smaller to the left
+        quickSort(array, start, indexPivot - 1);
+        // bigger to the right
+        quickSort(array, indexPivot + 1, end);
+    }
+}
+
+function partition(array, start, end) {
+    let pivot = array[end]; // choose pivot element
+    let indexPivot = start; // Index of smaller element and indicates the right position
+
+    for (let i = start; i < end; i++) {
+        if (array[i] <= pivot) {
+            swap(array, i, indexPivot);
+            indexPivot++;
+        }
+    }
+    swap(array, end, indexPivot); // swap pivot to right position
+
+    return indexPivot;
+}
+
+function swap(array, first, second) {
+    let temp = array[first];
+    array[first] = array[second];
+    array[second] = temp;
+}
+quickSort(numbers, 0, numbers.length - 1)
+console.log(numbers);
