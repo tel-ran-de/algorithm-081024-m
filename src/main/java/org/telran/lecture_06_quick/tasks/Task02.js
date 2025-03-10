dfcadb7f66796e560679f16b93a8a0c846362467
Sergej Hofmann <152594101+HofmannS@users.noreply.github.com>
2025-03-10 11:49:19 +0100

    Update Task02.js

--- src/main/java/org/telran/lecture_06_quick/tasks/Task02.js
+++ src/main/java/org/telran/lecture_06_quick/tasks/Task02.js
@@ -3,3 +3,12 @@
 // Найти: сумму 5-ти самых больших элементов по модулю.
 // Пояснение: сравниваем элементы по модулю, а в сумму добавляем сами значения элементов(без модуля)
 // В примере ниже, два самых больших по модулю числа это: -224 и 211. Они самые большие по модулю, а их сумма = -13
+function sumFiveModNumbers(array){
+    let modSortNum = array.sort((a, b) => Math.abs(a) - Math.abs(b))
+    let modSortNum5 = modSortNum.slice(-5)
+    let sum = 0
+   for (let i = 0; i < modSortNum5.length; i++) {
+    sum += modSortNum5[i]
+   }
+    return sum
+}
