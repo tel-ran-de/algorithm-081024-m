0fc0edd038fe3372f67d15937e3d631ecf6d6927
Halaktionova Olha <galaktionovaolga555@gmail.com>
2025-03-10 09:42:46 +0200

    Update Task03.js

--- src/main/java/org/telran/lecture_05_merge/home_work/Task03.js
+++ src/main/java/org/telran/lecture_05_merge/home_work/Task03.js
@@ -15,3 +15,47 @@
 // Возможен такой порядок: 10 2 50 1 10
 
 let prices = [2, 1, 10, 50, 10]; // Цены товаров которые вы купили
+function mergeSort(arr) { // сортировка слиянием
+    if (arr.length <= 1) {
+        return arr;
+    }
+    let mid = Math.floor(arr.length / 2); 
+    let leftArr = arr.slice(0, mid);
+    let rightArr = arr.slice(mid);
+    return merge(mergeSort(leftArr), mergeSort(rightArr));
+}
+
+function merge(left, right) {
+    let result = [];
+    i = 0
+    j = 0
+    while(i < left.length && j <right.length){
+        if (left[i] <= right[j]){
+            result.push(left[i])
+            i++
+        }else{
+            result.push(right[j])
+            j++
+        }
+    }
+    while(i < left.length){
+        result.push(left[i])
+        i++
+    }
+    while (j < right.length) {
+        result.push(right[j]);
+        j++;
+    }
+       return result;
+}
+function totalPrice(arr) {
+    let sortArr = mergeSort(arr)
+    let sum = 0
+    let mid = Math.floor(sortArr.length / 2)
+    let biggerPriceArr = sortArr.slice(mid)// берет элементы массива с середины до конца
+    for (let i = 0; i < biggerPriceArr.length; i++) {
+        sum +=biggerPriceArr[i]        
+    }
+    return sum       
+}
+console.log(totalPrice(prices));
