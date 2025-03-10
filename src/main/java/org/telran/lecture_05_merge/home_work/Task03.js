e38812f59a3cd7b814682ae98944d6c6b5e17cbf
IrinAnoVo <irinapavelviktor@gmail.com>
2025-03-10 09:22:51 +0100

    Update Task03.js

--- src/main/java/org/telran/lecture_05_merge/home_work/Task03.js
+++ src/main/java/org/telran/lecture_05_merge/home_work/Task03.js
@@ -14,4 +14,16 @@
 // Пояснение:
 // Возможен такой порядок: 10 2 50 1 10
 
+function calculateMaxReceiptSum(prices) {
+    let sum = 0;   
+    while (prices.length > 0) {
+        let maxIndex = prices.indexOf(Math.max(...prices));
+        sum += prices[maxIndex];
+        prices.splice(maxIndex, 1);
+        if (prices.length > 0) {
+            let minIndex = prices.indexOf(Math.min(...prices));
+            prices.splice(minIndex, 1);
+        }
+    }
+
 let prices = [2, 1, 10, 50, 10]; // Цены товаров которые вы купили
